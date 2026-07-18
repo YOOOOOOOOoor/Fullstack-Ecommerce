4a709ab1adb87810abeba96c38efb113877590d2
YOOOOOOOOoor <tyordi111@gmail.com>
2026-07-18 12:34:31 +0300

    login issue

--- Backend/controllers/authControllers.js
+++ Backend/controllers/authControllers.js
@@ -23,7 +23,7 @@ const clearCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-  // partitioned: process.env.NODE_ENV === "production",
+  path: "/",
 };
 
 const tokenGenerate = (id, role) => {
